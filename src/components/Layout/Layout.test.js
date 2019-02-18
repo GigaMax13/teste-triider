f6f9d8670825e7882c6da0b6dfcf881d5b50fc9b
GigaMax13 <gigamax13@gmail.com>
2019-02-18 17:04:40 -0300

    Adiciona teste do componente Layout

---
+++ src/components/Layout/Layout.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { Layout } from './Layout';
+
+describe('Layout', () => {
+  it('deve renderizar o componente', () => {
+    const wrapper = shallow(<Layout/>);
+
+    expect(wrapper.exists()).toBeTruthy();
+  });
+});
